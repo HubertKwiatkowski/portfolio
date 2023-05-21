bb9e22be3d3b0adf46afd7355c687fdaed650265
Hubert <h.kwiatkowski@hotmail.com>
2023-05-21 12:26:23 +0200

    chore: changed cv rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React from "react";
-import { Footer, Navbar } from "./components";
+import React, { useState } from "react";
+import { Cv, Footer, Navbar } from "./components";
 import { AboutMe, Projects } from "./container";
 
 import "./App.css";
 
-const App = () => (
-  <div className="app-wrapper">
-    <Navbar />
-    <div className="content-wrapper">
-      <AboutMe />
-      <Projects />
-      <Footer />
+const App = () => {
+  const [showCv, changeShowCv] = useState(false);
+
+  const toggleShowCv = () => {
+    changeShowCv(!showCv)
+  }
+
+  return (
+    <div className="app-wrapper">
+      {showCv && (
+        <div>
+          <Navbar onChangeShow={toggleShowCv} />
+          <div className="content-wrapper">
+            <AboutMe />
+            <Projects />
+            <Footer />
+          </div>
+        </div>
+      )}
+      {!showCv && (
+        <div>
+          <div className="btn-wrapper">
+            <button className="btn close-modal" onClick={() => {}}>
+              PDF
+            </button>
+            <button className="btn close-modal" onClick={toggleShowCv}>
+              Close
+            </button>
+          </div>
+          <Cv />
+        </div>
+)}
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
